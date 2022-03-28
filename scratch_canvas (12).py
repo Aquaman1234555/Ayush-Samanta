edaacca4963e9268e5ed8a42b6a79d7da2d1dd80
Aquaman1234555 <101315879+Aquaman1234555@users.noreply.github.com>
2022-03-28 11:20:47 +0530

    Add files via upload

---
+++ scratch_canvas (12).py
@@ -0,0 +1,19 @@
+n1 = int(input('Enter the number 1'))
+n2 = int(input('Enter the number 2'))
+def addition(n1, n2):
+    print('The addition of two numbers')
+    print(n1 + n2)
+def subtraction(n1, n2):
+    print('The subtraction of two numbers')
+    print(n1 - n2)
+def multiplication(n1, n2):
+    print('The multiplication of two numbers')
+    print(n1 * n2)
+def division(n1, n2):
+    print('The division of two numbers')
+    print(n1 / n2)
+
+addition(n1, n2)
+subtraction(n1, n2)
+multiplication(n1, n2)
+division(n1, n2)
